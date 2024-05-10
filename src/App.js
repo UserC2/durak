2a5fc57828e675d1743e623589f3576d5d63565f
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-05-10 10:12:50 -0400

    Add title screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Settings from './Settings';
+import {Button} from '@mui/material'
 
 export default function App() {
   return (
-    <div className='App'>
+    <div className="App">
+      <head>
+        <title>Durak</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </head>
+      <Title/ >
       <Settings/ >
+    </div>
+  );
+}
+
+function Title() {
+  return (
+    <div className="TitleScreen">
+      <h1>Durak</h1>
+      <Button variant="contained">Start</Button>
     </div>
   );
 }
